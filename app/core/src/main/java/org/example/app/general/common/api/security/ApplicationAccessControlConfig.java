3cafc9e0ea5f925afaf156d57c664ed7a0fa8fa1
Jörg Hohwiller <hohwille@users.sourceforge.net>
2021-05-21 12:32:15 +0200

    access-control added

--- app/core/src/main/java/org/example/app/general/common/api/security/ApplicationAccessControlConfig.java
+++ app/core/src/main/java/org/example/app/general/common/api/security/ApplicationAccessControlConfig.java
@@ -17,7 +17,21 @@ public class ApplicationAccessControlConfig extends AccessControlConfig {
 
   public static final String GROUP_READ_MASTER_DATA = PREFIX + "ReadMasterData";
 
-  public static final String GROUP_ADMIN = PREFIX + "Admin";
+  public static final String PERMISSION_FIND_ORDER = PREFIX + "FindOrder";
+
+  public static final String PERMISSION_SAVE_ORDER = PREFIX + "SaveOrder";
+
+  public static final String PERMISSION_DELETE_ORDER = PREFIX + "DeleteOrder";
+
+  public static final String PERMISSION_FIND_ITEM = PREFIX + "FindItem";
+
+  public static final String PERMISSION_SAVE_ITEM = PREFIX + "SaveItem";
+
+  public static final String PERMISSION_DELETE_ITEM = PREFIX + "DeleteItem";
+
+  public static final String GROUP_ADMIN = "admin";
+
+  public static final String GROUP_WAITER = "waiter";
 
   /**
    * The constructor.
@@ -25,8 +39,9 @@ public class ApplicationAccessControlConfig extends AccessControlConfig {
   public ApplicationAccessControlConfig() {
 
     super();
-    AccessControlGroup readMasterData = group(GROUP_READ_MASTER_DATA);
-    group(GROUP_ADMIN, readMasterData);
+    AccessControlGroup readMasterData = group(GROUP_READ_MASTER_DATA, PERMISSION_FIND_ITEM, PERMISSION_FIND_ORDER);
+    AccessControlGroup waiter = group(GROUP_WAITER, readMasterData, PERMISSION_SAVE_ITEM, PERMISSION_SAVE_ORDER);
+    group(GROUP_ADMIN, waiter, PERMISSION_DELETE_ITEM, PERMISSION_DELETE_ORDER);
   }
 
 }
